20b813e634047183d9bf7708031c9ce8982ee13b
career222 <gadedipak1111@gmail.com>
2026-09-16 22:47:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let phoneNumber = document.getElementById("phoneNumber").value;
+    let emailId = document.getElementById("emailId").value;
+
+    alert(
+        "First Name: " + firstName + " " +
+        "Last Name: " + lastName + " " +
+        "Phone Number: " + phoneNumber + " " +
+        "Email ID: " + emailId
+    );
+});
